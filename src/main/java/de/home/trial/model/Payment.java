b91c786d8a72aedea81a372b547058899f196635
c.sterr <c.sterr@leverton.de>
2018-03-22 12:34:20 +0100

    cleanup

--- src/main/java/de/home/trial/model/Payment.java
+++ src/main/java/de/home/trial/model/Payment.java
@@ -2,7 +2,6 @@ package de.home.trial.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Payment {
     Long sum = 0L;
@@ -23,23 +22,4 @@ public class Payment {
             this.sum = this.items.stream().filter(item -> !item.getDeleted()).map(item -> item.value).reduce(Long::sum).get();
     }
 
-    public void addItem(Item item) {
-        this.sum += item.value;
-        this.items.add(item);
-    }
-
-    public void removeItem(Long id) {
-        Optional<Item> itemToRemove = this.items.stream().filter(item -> item.id.equals(id)).findFirst();
-        Item item = itemToRemove.get();
-        this.sum = this.sum - item.value;
-        items.remove(itemToRemove.get());
-    }
-
-    public Long getSum() {
-        return sum;
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
 }
